05e0e2e7b85c6ce4fea49c25f70ede18c68462f3
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-10-27 23:42:04 -0400

    Initial push

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,8 @@
+from argparse import _AppendAction
+from os import remove
+from typing import Text
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,7 +12,35 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as text:
+        text = text.read()
+        clean_text = text.replace("\n", "").replace(".", "").replace("’", "").replace(
+            ";", "").replace("_", "").replace(",", "").lower().split(' ')
+        for word in clean_text:
+            if word in STOP_WORDS and clean_text:
+                clean_text.remove(word)
+
+
+    print(clean_text)
+
+    # .count(clean_text)
+
+
+# def print_word_freq(file):
+#     """Read in `file` and print out the frequency of words in that file."""
+#     with open(file) as text:
+#         lines = text.readlines()
+
+#         for line in lines:
+#             line = line.replace(".", " ")
+#             line = line.replace(chr(39), " ")
+#             line = line.replace(";", " ")
+#             line = line.replace("_", " ")
+#             line = line.replace(",", " ")
+#             line = line.lower()
+#             line = line.split(' ')
+#     print(line)
+#     print(lines)
 
 
 if __name__ == "__main__":
